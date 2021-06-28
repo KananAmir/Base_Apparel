28d59845064d179a583ba41b22e42409a28fcba1
Amirov Kanan <amirovknn@gmail.com>
2021-06-28 17:48:46 +0400

    second commit

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ btn.addEventListener('click', function(){
     if(input.nodeValue != '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$'){
         error_img.style.display = 'block';
         invalid.innerHTML = "Please povide a valid email"
+        input.style.border = '2px solid red'
+
     }
     else{
         error_img.style.display = 'none';
